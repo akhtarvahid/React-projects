d284502fbee9f31bb01ef84402e55e3e9d9d1b32
Vahid Akhtar <akhtarvahid543@gmail.com>
2022-05-29 22:37:42 +0530

    calculator ui row created

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import Button from './components/Button';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-       Calculator
+       <div className="calc-wrapper">
+         <div className="row">
+          <Button>7</Button>
+          <Button>8</Button>
+          <Button>9</Button>
+          <Button>/</Button>
+         </div>
+       </div>
     </div>
   );
 }
--- calculator/src/components/Button.js
+++ calculator/src/components/Button.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import './Button.css';
 
 const isOperator = value => {
     return !isNaN(value) || value === '.' || value === '=';
